2683f383f8012c5fcedfb20318129feaf7f4b01d
Bryan Brinez <bryan-26-8@hotmail.com>
2024-07-20 01:30:05 -0500

    route /user/register and zod type user added

--- src/app/api/firebase/config.js
+++ src/app/api/firebase/config.js
@@ -1,5 +1,7 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -13,5 +15,6 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 
 const auth = getAuth(app)
+const db = getFirestore(app);
 
-export {app, auth}
+export { app, auth, db };
---
+++ src/app/api/user/register/route.js
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+import { auth, db } from "../../firebase/config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { setDoc, doc } from "firebase/firestore";
+import { UsuarioSchema } from "../../../types/UserSchema";
+
+export async function POST(request) {
+  const userData = await request.json();
+
+  try {
+    // Validar los datos del usuario usando el esquema de Zod
+    UsuarioSchema.parse(userData);
+
+    // Crear usuario en Firebase Auth
+    const userCredential = await createUserWithEmailAndPassword(auth, userData.email, userData.password);
+    const user = userCredential.user;
+
+    // Guardar los datos adicionales en Firestore
+    
+    await setDoc(doc(db, 'users', user.uid), {
+      
+      nombre: userData.nombre,
+      apellido: userData.apellido,
+      email: userData.email,
+      programa_asignado: userData.programa_asignado,
+      tipo_identificacion: userData.tipo_identificacion,
+      numero_identificacion: userData.numero_identificacion,
+      codigo: userData.codigo,
+      rol: userData.rol,
+      sede: userData.sede,
+      tel: userData.tel,
+    });
+
+    return NextResponse.json({ uid: user.uid, email: user.email });
+  } catch (error) {
+    if (error.errors) {
+      // Errores de validación de Zod
+      return NextResponse.json({ message: error.errors }, { status: 400 });
+    } else {
+      // Otros errores (Firebase Auth)
+      return NextResponse.json({ message: error.message }, { status: 400 });
+    }
+  }
+}
