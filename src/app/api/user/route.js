940ade81316630325919246accf43f8c74f841fb
Bryan Martinez Briñez <94941570+BryanBrinez@users.noreply.github.com>
2024-07-24 23:44:27 -0500

    Update route.js

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,18 +1,27 @@
-
 import { NextResponse } from "next/server";
-import { db } from "../firebase/config"
+import { db } from "../firebase/config";
 import { collection, getDocs } from "firebase/firestore";
 
 export async function GET() {
-    try {
-      const usersCollection = collection(db, "users");
-      const userSnapshot = await getDocs(usersCollection);
-      const userList = userSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-  
-      return new Response(JSON.stringify(userList));
-    } catch (error) {
-      return new Response(JSON.stringify({ message: error.message }), {
-        status: 400,
-      });
-    }
+  console.log("GET request received");
+  try {
+    const usersCollection = collection(db, "users");
+    const userSnapshot = await getDocs(usersCollection);
+    const userList = userSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+    console.log("User list:", userList);
+    return new Response(JSON.stringify(userList), {
+      headers: {
+        'Cache-Control': 'no-store'
+      }
+    });
+  } catch (error) {
+    console.error('Error fetching users from Firestore:', error);
+    return new Response(JSON.stringify({ message: error.message }), {
+      status: 400,
+      headers: {
+        'Cache-Control': 'no-store'
+      }
+    });
   }
+}
