ffb8948154b969bfcf6144b827d82970f25314b4
Steven Victoria <102372427+TevenV27@users.noreply.github.com>
2024-09-11 10:43:56 -0500

    ADD Modal to create program

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,31 @@
-import Image from "next/image";
+'use client';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { Loader } from 'rsuite';
 
 export default function Home() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Redirige a la ruta deseada cuando el componente se monta
+    const redirect = async () => {
+      try {
+        await router.push('/home/area-personal');
+      } finally {
+        setLoading(false);
+      }
+    };
+    redirect();
+  }, [router]);
+
   return (
-    <main >
-     <div> Hola</div>
+    <main className='h-screen w-screen flex justify-center items-center'>
+      {loading ? (
+        <Loader size="lg" />
+      ) : (
+        <div>SERA UNIVALLE</div>
+      )}
     </main>
   );
 }
